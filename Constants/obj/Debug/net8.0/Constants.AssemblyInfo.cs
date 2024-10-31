420a5be37ace417e9573143486274d68edcd79c8
alex <aezayat@email.neit.edu>
2024-10-31 06:50:04 -0400

    update

--- Constants/obj/Debug/net8.0/Constants.AssemblyInfo.cs
+++ Constants/obj/Debug/net8.0/Constants.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Constants")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6155e2f0d7a31cf28b1f1917decef352afbcbb4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fee4761c7d99eef2f7b3afd83bf60379a6dbd00")]
 [assembly: System.Reflection.AssemblyProductAttribute("Constants")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Constants")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- returns/obj/Debug/net8.0/returns.AssemblyInfo.cs
+++ returns/obj/Debug/net8.0/returns.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("returns")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6155e2f0d7a31cf28b1f1917decef352afbcbb4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fee4761c7d99eef2f7b3afd83bf60379a6dbd00")]
 [assembly: System.Reflection.AssemblyProductAttribute("returns")]
 [assembly: System.Reflection.AssemblyTitleAttribute("returns")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
