5c5aff8008d82efb9ab2e18db3942b2beb01cfd6
alex <aezayat@email.neit.edu>
2024-11-07 11:06:02 -0500

    update

--- methods/obj/Debug/net8.0/methods.AssemblyInfo.cs
+++ methods/obj/Debug/net8.0/methods.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("methods")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+420a5be37ace417e9573143486274d68edcd79c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16e59b215e93314d62847e89b49a0c4e503f24ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("methods")]
 [assembly: System.Reflection.AssemblyTitleAttribute("methods")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
